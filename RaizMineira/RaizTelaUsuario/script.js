d90ca913498a8a340da89b8f9e05855d5650b134
Srmarlongs <marlong2008silva@gmail.com>
2025-06-24 16:43:02 -0300

    Atualização TCC 24/06

---
+++ RaizMineira/RaizTelaUsuario/script.js
@@ -0,0 +1,13 @@
+// Voltar para a página anterior
+document.getElementById('voltar-reservas')
+  .addEventListener('click', () => {
+    window.history.back();
+  });
+
+// Exemplos de uso adicional (SweetAlert2) se você precisar:
+// document.getElementById('btn-ver-reservas').addEventListener('click', () => {
+//   Swal.fire('A lista de reservas ainda não foi implementada.');
+// });
+// document.getElementById('btn-cadastrar-reserva').addEventListener('click', () => {
+//   Swal.fire('O formulário de cadastro ainda não foi implementado.');
+// });
