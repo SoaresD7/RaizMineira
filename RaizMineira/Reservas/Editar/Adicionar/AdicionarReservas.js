5a6906cc68b778efabfd8b87b00f4592f4de5330
Cyan Stars <cyanstars123@hotmail.com>
2025-07-07 16:10:30 -0300

    Delete RaizMineira directory

--- RaizMineira/Reservas/Editar/Adicionar/AdicionarReservas.js
+++
@@ -1,6 +0,0 @@
-// JS para Adicionar Reserva
-// Adicione aqui a lógica de validação e envio futuramente
-
-document.querySelector('.botao-voltar').addEventListener('click', function() {
-  window.history.back();
-});
