028d4d2f476481e0962ef9f1a65c721f3ff86f95
Davi Soares <Davi.soares2008@icloud.com>
2025-08-04 19:47:25 -0300

    Delete TCC directory

--- TCC/Gerente/Cardapio/CardapioJS.js
+++
@@ -1,3 +0,0 @@
-function voltar() {
-  window.location.href = "../Main.html";
-}
--- TCC/Gerente/Reservas/Editar/Adicionar/EditarReservas.js
+++
@@ -1,6 +0,0 @@
-// JS para Editar Reserva
-// Adicione aqui a lógica de validação e envio futuramente
-
-document.querySelector('.botao-voltar').addEventListener('click', function() {
-  window.history.back();
-});
